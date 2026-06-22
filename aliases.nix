6f166bf12597735fb4a1d4b5128eeffdcbfd05e2
Illia Diadenchuk <dadencukillia@gmail.com>
2026-06-22 16:59:52 +0300

    feat: nixos snapshots clear alias

--- aliases.nix
+++ aliases.nix
@@ -1,7 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   environment.shellAliases = {
     nixsw = "nixos-rebuild switch --flake \"path:/etc/nixos#linuxSystem\"";
+    nixcl = "nix-collect-garbage -d";
   };
 }
