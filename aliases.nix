4f31efb9425c2cd65315af7f72cd1546fbbd556a
Illia Diadenchuk <dadencukillia@gmail.com>
2026-06-25 16:27:11 +0300

    feat: helpful alias for nixos adjustment

--- aliases.nix
+++ aliases.nix
@@ -4,5 +4,6 @@
   environment.shellAliases = {
     nixsw = "nixos-rebuild switch --flake \"path:/etc/nixos#linuxSystem\"";
     nixcl = "nix-collect-garbage -d";
+    nixcfg = "sudo sh -c \"cd /etc/nixos && nvim .\"";
   };
 }
