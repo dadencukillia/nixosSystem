fd9efccd31f3f280aaa9063ed19e55c44d0857f9
Illia Diadenchuk <dadencukillia@gmail.com>
2026-06-25 22:15:24 +0300

    fix: git invalid personal config options

--- dot/personalOptions/git.nix
+++ dot/personalOptions/git.nix
@@ -7,10 +7,8 @@
       user.name = "Illia Diadenchuk";
       user.email = "dadencukillia@gmail.com";
 
-      extraConfig = {
-        init.defaultBranch = "main";
-        core.editor = "nvim";
-      };
+      init.defaultBranch = "main";
+      core.editor = "nvim";
     };
   };
 }
