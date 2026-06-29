261d8bbc7c62df5f7f55ed7df1797bfc0e9d9a76
Illia Diadenchuk <dadencukillia@gmail.com>
2026-06-29 22:08:55 +0300

    feat(alias): a convenient way to launch thunar right in pwd

--- aliases.nix
+++ aliases.nix
@@ -5,5 +5,6 @@
     nixsw = "nixos-rebuild switch --flake \"path:/etc/nixos#linuxSystem\"";
     nixcl = "nix-collect-garbage -d";
     nixcfg = "sudo sh -c \"cd /etc/nixos && nvim .\"";
+    thunar-here = "thunar . & disown";
   };
 }
